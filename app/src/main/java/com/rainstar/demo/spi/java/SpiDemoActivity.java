7cec09bdff740b12c090c4448cd3e64de491127b
chuangjie.pcj <panchuangjie@163.com>
2023-07-13 17:15:35 +0800

    opt: 删除多余的导包

--- app/src/main/java/com/rainstar/demo/spi/java/SpiDemoActivity.java
+++ app/src/main/java/com/rainstar/demo/spi/java/SpiDemoActivity.java
@@ -1,8 +1,5 @@
 package com.rainstar.demo.spi.java;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
